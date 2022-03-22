bab63bb87f2df499598dd9249c9cfac08128c35e
Sadid369 <sadid.jones@gmail.com>
2022-03-23 01:13:14 +0600

    ....

--- blackjack.py
+++ blackjack.py
@@ -40,21 +40,8 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
-
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-user_cards = []
-computer_cards = []
-
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-    
-print(computer_cards)
-print(user_cards)
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
-#Look up the sum() function to help you do this.
 def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards"""
     # if 11 in cards and 10 in cards and len(cards) ==2:
     if sum(cards) == 21 and len(cards) == 2:
         return 0 
@@ -62,6 +49,24 @@ def calculate_score(cards):
         cards.remove(11)
         cards.appends(1)
     return sum(cards)
+#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+user_cards = []
+computer_cards = []
+is_game_over = False
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+user_score= calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)  
+print(f"Your card: {user_cards}, current score: {user_score}")
+print(f"Computer first card: {computer_cards[0]}")
+ 
+if user_score == 0 or computer_score == 0 or user_score>21:
+    is_game_over = True
+#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
+#and returns the score. 
+#Look up the sum() function to help you do this.
+
     
 
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
